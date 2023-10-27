5d76456480b5d77553954ece63bf7b939b9bff05
Nicholai <nichobech02@gmail.com>
2023-10-27 12:37:32 +0200

    UBER AUTISM, OBSERVE

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="bg-blue-500 p-4">
+        <h1 className="text-2xl text-white">Hello, Tailwind CSS!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
